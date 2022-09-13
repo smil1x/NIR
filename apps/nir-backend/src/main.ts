98ee04ba670211064f52a598255899bdc12bd432
aleksandr_pokrovsky <smil1x@mail.ru>
2022-09-13 16:00:59 +0300

    add swagger module

--- apps/nir-backend/src/main.ts
+++ apps/nir-backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import {ConfigService} from "@app/config";
 import {configOptions} from "./config-options.provider";
 import {BaseConfigInterface} from "./core/interfaces";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const configService = new ConfigService(configOptions);
@@ -11,6 +12,14 @@ async function bootstrap() {
 
   app.enableCors();
   app.setGlobalPrefix('api');
+
+  const swaggerConfig = new DocumentBuilder()
+      .setTitle('NIR')
+      .setDescription('NIR backend')
+      .setVersion('0.1.0')
+      .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(config.APP_PORT || 3000);
 }
--- apps/nir-backend/src/marinetraffic/marinetraffic.controller.ts
+++ apps/nir-backend/src/marinetraffic/marinetraffic.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { MarinetrafficService } from './marinetraffic.service';
+import {ApiOkResponse, ApiTags} from "@nestjs/swagger";
 
+@ApiTags('Marinetraffic')
 @Controller()
 export class MarinetrafficController {
   constructor(private readonly appService: MarinetrafficService) {}
 
   @Get()
+  @ApiOkResponse({
+    description: 'Successfully',
+    type: String,
+  })
   getHello(): string {
     return this.appService.getHello();
   }
