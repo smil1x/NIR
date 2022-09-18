6ca53c74b9a13469a5f36b809a2dfbdde139a895
aleksandr_pokrovsky <smil1x@mail.ru>
2022-09-19 01:17:52 +0300

    - add getVesselHistoricalPositions method

--- apps/nir-backend/src/marinetraffic/marinetraffic.controller.ts
+++ apps/nir-backend/src/marinetraffic/marinetraffic.controller.ts
@@ -1,18 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { MarinetrafficService } from './marinetraffic.service';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Marinetraffic')
 @Controller()
 export class MarinetrafficController {
   constructor(private readonly appService: MarinetrafficService) {}
 
-  @Get()
+  @Get(':shipid')
+  @ApiOperation({
+    summary: 'Get Vessel Position',
+  })
   @ApiOkResponse({
     description: 'Successfully',
     type: String,
   })
-  getSingleVesselPositions(): string {
-    return this.appService.getSingleVesselPositions();
+  async getSingleVesselPositions(
+    @Param('shipid') shipid: number,
+  ): Promise<any> {
+    return await this.appService.getSingleVesselPositions(shipid);
+  }
+
+  @Get(':shipid/history')
+  @ApiOperation({
+    summary: 'Get Vessel Historical Positions',
+  })
+  @ApiOkResponse({
+    description: 'Successfully',
+    type: String,
+  })
+  async getVesselHistoricalPositions(
+    @Param('shipid') shipid: number,
+  ): Promise<any> {
+    return await this.appService.getVesselHistoricalPositions(shipid);
   }
 }
--- apps/nir-backend/src/marinetraffic/marinetraffic.service.ts
+++ apps/nir-backend/src/marinetraffic/marinetraffic.service.ts
@@ -7,25 +7,48 @@ export class MarinetrafficService {
   constructor(
     @Inject(MARINETRAFIC_KEY) protected readonly marinetrafficKey: string,
   ) {}
-  getSingleVesselPositions(): string {
-    axios
+
+  async getSingleVesselPositions(shipid: number): Promise<any> {
+    return axios
       .get(
         `https://services.marinetraffic.com/api/exportvessel/${this.marinetrafficKey}`,
         {
           params: {
             v: 1,
-            shipid: 281897,
+            shipid: shipid,
             protocol: 'json',
           },
         },
       )
       .then((res) => {
         console.log(res.data);
+        return res.data;
       })
       .catch((err) => {
         console.log(err);
       });
-    console.log(this.marinetrafficKey);
-    return `It is nir-backend app ${this.marinetrafficKey}`;
+  }
+
+  async getVesselHistoricalPositions(shipid: number): Promise<any> {
+    return axios
+      .get(
+        `https://services.marinetraffic.com/api/exportvesseltrack/29d511fafdb93d5c9fad0f37667573c04e2e1fbb`,
+        {
+          params: {
+            v: 1,
+            shipid: shipid,
+            days: 1,
+            protocol: 'json',
+            period: 'daily',
+          },
+        },
+      )
+      .then((res) => {
+        console.log(res.data);
+        return res.data;
+      })
+      .catch((err) => {
+        console.log('errrrrrrrrrrrrrrrrr', err.response.data);
+      });
   }
 }
